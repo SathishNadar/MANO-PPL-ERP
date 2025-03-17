aa197797ddb68ae0fc4ece1fcb6c13c815499667
Mano-Bharathii <manobharathi189@gmail.com>
2025-03-17 21:55:22 +0530

    Changed Dotenv configuration method

--- backend/routes/database.js
+++ backend/routes/database.js
@@ -1,14 +1,9 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 import Fuse from "fuse.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Load environment variables
-const result = dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+const result = dotenv.config();
 
 if (result.error) {
     console.error("Error loading .env file:", result.error);
