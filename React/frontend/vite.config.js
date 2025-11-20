e6eb169828de01926ee1d03c9e8c4acb30a3b2fa
SathishNadar <sathishmacbook01@gmail.com>
2025-11-20 14:34:29 +0530

    changes to vite config file to accomodate https reqs

--- React/frontend/vite.config.js
+++ React/frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   server: {
-    host: '0.0.0.0',
-  },
-})
+    host: true,        // accept network requests (and custom Host headers)
+    port: 5173,
+    hmr: {
+      // helpful if you want HMR to work when accessing via domain
+      host: "erp.mano.co.in",
+    }
+  }
+});
