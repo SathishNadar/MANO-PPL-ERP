459fe2476eddd36976afbb3c12e43ec1ab2d2f66
SathishNadar <sathishmacbook01@gmail.com>
2025-11-22 12:12:58 +0530

    changes to vite config, index.html and added package dependencies

--- React/frontend/src/main.jsx
+++ React/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
+import 'material-icons/iconfont/material-icons.css'
 import App from './App.jsx'
 import './index.css';
 
--- React/frontend/vite.config.js
+++ React/frontend/vite.config.js
@@ -1,20 +1,18 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // optional but recommended
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',    // accept LAN connections
-    port: 5173,
-    strictPort: false,
-    // origin helps Vite generate correct absolute URLs for the client
-    origin: 'https://erp.mano.co.in',
-    hmr: {
-      protocol: 'wss',         // client will use wss
-      host: 'erp.mano.co.in',  // websocket endpoint host (nginx will proxy)
-      port: 443,               // client connects to 443 (TLS) — nginx should accept wss on 443
-      clientPort: 443
-    },
-    cors: true
-  }
+  host: '0.0.0.0',
+  port: 5173,
+  hmr: {
+    protocol: 'wss',
+    host: 'erp.mano.co.in',
+    port: 443,
+    clientPort: 443
+  },
+  cors: true
+}
 })
