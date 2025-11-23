10769d95113e528e711c1241e0ffbc428ac5c134
SathishNadar <sathishmacbook01@gmail.com>
2025-11-23 20:22:14 +0530

    vite config file change

--- React/frontend/vite.config.js
+++ React/frontend/vite.config.js
@@ -1,36 +1,36 @@
-// frontend/vite.config.js
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//   host: '0.0.0.0',
-//   port: 5173,
-//   hmr: {
-//     protocol: 'wss',
-//     host: 'erp.mano.co.in',
-//     clientPort: 443
-//   },
-//   cors: true
-// }
-// })
-
-// vite.config.js — local dev friendly
+frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',   // accept LAN if you want to test from another device
-    port: 5173,
-    hmr: {
-      protocol: 'ws',    // plain ws on local dev
-      host: 'localhost', // or your machine IP if testing from other device
-      port: 5173,        // match vite dev server port
-      clientPort: 5173
-    },
-    cors: true
-  }
+  host: '0.0.0.0',
+  port: 5173,
+  hmr: {
+    protocol: 'wss',
+    host: 'erp.mano.co.in',
+    clientPort: 443
+  },
+  cors: true
+}
 })
+
+// // vite.config.js — local dev friendly
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',   // accept LAN if you want to test from another device
+//     port: 5173,
+//     hmr: {
+//       protocol: 'ws',    // plain ws on local dev
+//       host: 'localhost', // or your machine IP if testing from other device
+//       port: 5173,        // match vite dev server port
+//       clientPort: 5173
+//     },
+//     cors: true
+//   }
+// })
